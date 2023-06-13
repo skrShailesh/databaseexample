23a57424439fcd48742b6e3438ec6f1289cebc33
Shailesh kumar <skrshailesh1996@gmail.com>
2023-06-13 21:40:52 +0530

    Initial commit

---
+++ app/src/main/java/com/example/databaseexample/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.databaseexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        MyDBHelper dbHelper = new MyDBHelper(this);
+        dbHelper.addContact( "Raman", "9999999999");
+        dbHelper.addContact( "Shubham", "8899999999");
+        dbHelper.addContact( "Rahul", "7799999999");
+        dbHelper.addContact( "Raman", "6699999999");
+
+    }
+}
---
+++ app/src/main/java/com/example/databaseexample/MyDBHelper.java
@@ -0,0 +1,49 @@
+package com.example.databaseexample;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.os.Build;
+
+import androidx.annotation.Nullable;
+
+public class MyDBHelper extends SQLiteOpenHelper {
+    private static final String DATABASE_NAME = "ContastsDB";
+    private static final int DATABASE_VERSION = 1;
+    private static final String TABLE_CONTACT = "contacts";
+    private static final String KEY_ID = "id";
+    private static final String KEY_NAME = "name";
+    private static final String KEY_PHONE_NO = "phone_no";
+
+    public MyDBHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+        db.execSQL("CREATE TABLE " + TABLE_CONTACT +
+                "(" + KEY_ID + " INTEGER PRIMARY KEY AUTOINCREMENT," + KEY_NAME + "TEXT," + KEY_PHONE_NO + "TEXT" + " )");
+
+    }
+
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS "+ TABLE_CONTACT);
+
+        onCreate(db);
+
+    }
+    public void addContact(String name, String phone_no){
+
+        SQLiteDatabase db = getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(KEY_NAME, name);
+        values.put(KEY_PHONE_NO, phone_no);
+
+        db.insert(TABLE_CONTACT, null, values);
+
+    }
+}
